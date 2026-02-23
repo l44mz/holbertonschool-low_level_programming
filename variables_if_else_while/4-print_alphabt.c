dea12b223b5fc186d90d2cb0510a92410b279f35
Lama <lamamqqz@gmail.com>
2026-02-23 19:03:53 +0000

    added 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets except e and q 
+ *        
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
