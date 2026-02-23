d2e83b13c21ebfb9e555a3e6511ee2c3d6714116
Lama <lamamqqz@gmail.com>
2026-02-23 18:52:20 +0000

    fixed

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char ch = 'a';
