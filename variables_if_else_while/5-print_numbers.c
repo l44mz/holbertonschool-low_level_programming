9a63760ac8c4eaae4fca9983ff5eb96ce4c3aa54
Lama <lamamqqz@gmail.com>
2026-02-23 19:14:34 +0000

    added 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d\n", num);
+	}	
+	return (0);
+}
