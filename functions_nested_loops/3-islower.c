0372b30f1d318c21f201711b73ac25ea8ad97523
Lama <lamamqqz@gmail.com>
2026-02-24 18:56:13 +0000

    added 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _islower - checks lowercase 
+ * _islower - checks lowercase
  *
  * Return: void
  */
 int _islower(int c)
 {
-	if(c >= 'a' && c<= 'z')
-		return(1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
 	else
-		return(0);
+		return (0);
 
 }
