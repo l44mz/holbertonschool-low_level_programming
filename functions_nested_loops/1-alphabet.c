f624bc24e7a09274423889e479cfc8b5018a3118
Lama <lamamqqz@gmail.com>
2026-02-24 18:01:16 +0000

    added 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include main.h 
+/**
+ * function that prints the alphabet, in lowercase, followed by a new line.
+ *
+ *
+ */
+void print_alphabet(void) {
+
+	ch letter = 'a';
+
+	for(letter = 'a'; letter <= 'z'; letter++){
+
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
