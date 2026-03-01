a3c3eef3a1d9fd5af37821b85ad9e6e5b6dbc05c
Lama <lamamqqz@gmail.com>
2026-03-01 19:00:34 +0000

    adde 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers,
+ * from 0 to 14, followed by a new line.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char c;
+
+	for (int a = 0; a < 11; a++)
+	{
+		for (c = '0'; c> '15'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+	}
+}
