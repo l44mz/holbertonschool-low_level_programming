484838a0b450b0f51994e93e6ebecb522b786be2
Lama <lamamqqz@gmail.com>
2026-03-01 19:25:08 +0000

    added 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: number of times the charactr _ is printed.
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n <= '0')
+	{
+		_putchar('\n');
+	}
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('_');
+
+	}
+	_putchar('\n');
+
+}
