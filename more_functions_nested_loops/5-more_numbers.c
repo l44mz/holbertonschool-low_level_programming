7c20293d7042343db652911b066e921bce5f0559
Lama <lamamqqz@gmail.com>
2026-03-01 19:10:05 +0000

    added 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,24 @@
 
 /**
  * more_numbers - prints 10 times the numbers,
- * from 0 to 14, followed by a new line.
+ * from 0 to 14, followed by a new line
  *
  * Return: void
  */
 void more_numbers(void)
 {
-	char c;
-	int a;
+	int i, j;
 
-	for (a = 0; a < 11; a++)
+	for (i = 0; i < 10; i++)
 	{
-		for (c = '0'; c> '15'; c++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(c);
+			if (j > 9)
+				_putchar('0' + (j / 10));
+
+			_putchar('0' + (j % 10))
 		}
 
 		_putchar('\n');
-
 	}
 }
