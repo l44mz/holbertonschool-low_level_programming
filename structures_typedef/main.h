e02319a41d5afd1f361689509155ea8412f19081
Lama <lamamqqz@gmail.com>
2026-03-29 13:37:30 +0000

    fixed

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,7 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif
+#endif /* MAIN_H */
