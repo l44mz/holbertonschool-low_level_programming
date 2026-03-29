2fa9c4c01b367123cfc34b12ccac877596b43eec
Lama <lamamqqz@gmail.com>
2026-03-29 13:17:04 +0000

    added readme and 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - represents a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
