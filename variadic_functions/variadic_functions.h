59a2f925d6463673a1085888e5c5646afb33cb1c
Lama <lamamqqz@gmail.com>
2026-04-09 13:11:47 +0000

    added 0

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of arguments
+ *
+ * Return: sum of all parameters
+ */
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
