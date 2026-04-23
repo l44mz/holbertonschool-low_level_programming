4b6d8c888422a43f85bc7ec174c5fefa888d9e13
Lama <lamamqqz@gmail.com>
2026-04-23 15:37:51 +0000

    fix: fixed betty style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,21 +8,19 @@
 
 /**
  * main - copies content from one file to another
- * @argc: argument count
- * @argv: argument vector
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w;
+	int fd_from, fd_to;
+	ssize_t r, w;
 	char buffer[BUF_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
@@ -37,29 +35,56 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDOUT_FILENO,
 			"Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
+		if (close(fd_from) == -1)
+		{
+			dprintf(STDOUT_FILENO,
+				"Error: Can't close fd %d\n", fd_from);
+			exit(100);
+		}
 		exit(99);
 	}
 
-	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
+	r = read(fd_from, buffer, BUF_SIZE);
+	while (r > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w == -1 || w != r)
 		{
 			dprintf(STDOUT_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
+			if (close(fd_from) == -1)
+			{
+				dprintf(STDOUT_FILENO,
+					"Error: Can't close fd %d\n", fd_from);
+				exit(100);
+			}
+			if (close(fd_to) == -1)
+			{
+				dprintf(STDOUT_FILENO,
+					"Error: Can't close fd %d\n", fd_to);
+				exit(100);
+			}
 			exit(99);
 		}
+		r = read(fd_from, buffer, BUF_SIZE);
 	}
 
 	if (r == -1)
 	{
 		dprintf(STDOUT_FILENO,
 			"Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
+		if (close(fd_from) == -1)
+		{
+			dprintf(STDOUT_FILENO,
+				"Error: Can't close fd %d\n", fd_from);
+			exit(100);
+		}
+		if (close(fd_to) == -1)
+		{
+			dprintf(STDOUT_FILENO,
+				"Error: Can't close fd %d\n", fd_to);
+			exit(100);
+		}
 		exit(98);
 	}
 
